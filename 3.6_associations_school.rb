d25c32404d3c2c6f31b8ff0dd86327f062348cdd
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-05-08 14:10:14 -0500

    3.6 Correct

--- 3.6_associations_school.rb
+++ 3.6_associations_school.rb
@@ -4,13 +4,62 @@ ActiveRecord::Schema.define do
   self.verbose = false
 
   # MIGRATIONS
-  # <-- your work goes here
+  create_table :students do |t|
+    t.string :name
+    t.string :contact_info
+  end
+
+  create_table :courses do |t|
+    t.string  :name
+    t.integer :teacher_id
+  end
+
+  create_table :grades do |t|
+    t.string  :letter
+    t.integer :percentage
+  end
+
+  create_table :teachers do |t|
+    t.string :name
+  end
+
+  create_table :enrollments do |t|
+    t.integer :student_id
+    t.integer :grade_id
+    t.integer :course_id
+  end
 end
 
 
 # MODELS
-# <-- your work goes here
+class Student < ActiveRecord::Base
+  has_many :enrollments
+  has_many :courses,  through: :enrollments
+  has_many :grades,   through: :enrollments
+  has_many :teachers, through: :courses
+end
 
+class Teacher < ActiveRecord::Base
+  has_many :courses
+  has_many :students, through: :courses
+end
+
+class Course < ActiveRecord::Base
+  belongs_to :teacher
+  has_many   :enrollments
+  has_many   :students, through: :enrollments
+end
+
+class Grade < ActiveRecord::Base
+  has_many :enrollments
+  has_many :students, through: :enrollments
+end
+
+class Enrollment < ActiveRecord::Base
+  belongs_to :student
+  belongs_to :course
+  belongs_to :grade
+end
 
 # TESTS
 class SchoolTest < Minitest::Test
